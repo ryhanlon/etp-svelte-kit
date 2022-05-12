8fd1fe3c4b4cbabfca6d77e1dff7c3509c1f1364
ryhanlon <ryhanlon@gmail.com>
2022-05-11 17:20:46 -0700

    Add 1.2.1 as test

--- src/routes/exploring-the-path/index.svelte
+++ src/routes/exploring-the-path/index.svelte
@@ -2,3 +2,4 @@
 <p>Chapter One</p>
 <a href="exploring-the-path/chapter-1/lesson-1-1-1">1.1.1</a>
 <a href="exploring-the-path/chapter-1/lesson-1-2-0">1.2.0</a>
+<a href="exploring-the-path/chapter-1/lesson-1-2-0">1.2.1</a>
