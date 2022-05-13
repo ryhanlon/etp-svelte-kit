ed20fead804ba1973c7af52e57f02f7165378193
ryhanlon <ryhanlon@gmail.com>
2022-05-12 20:28:04 -0700

    Add __layout, Header and Nav.  Test is working.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import '../styles/global.css';
+    import Nav from '$lib/Nav.svelte'
+  import Header from '$lib/Header.svelte'
+
+</script>
+  <Nav />
+  <Header />
+<slot></slot>
+
+<style></style>
